62e4edd4947a375ea525d182a48c31f5131b7482
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2023-08-08 11:05:30 -0300

    refactor: test_routine function name refactor

--- test/testsrc/test_ft_itoa.c
+++ test/testsrc/test_ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 09:46:17 by guferrei          #+#    #+#             */
-/*   Updated: 2023/08/08 09:56:08 by guferrei         ###   ########.fr       */
+/*   Updated: 2023/08/08 11:03:39 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <string.h>
 #include <limits.h>
 
-void	test_routine(char * expected, int test_value) {
+void	ft_itoa_test_routine(char * expected, int test_value) {
 	char*	actual;
 
 	actual = ft_itoa(test_value);
@@ -23,34 +23,34 @@ void	test_routine(char * expected, int test_value) {
 }
 
 void	ft_itoa_positive_numbers_tests(void) {
-	test_routine("0", 0);
-	test_routine("42", 42);
-	test_routine("666", 666);
-	test_routine("2112", 2112);
-	test_routine("54321", 54321);
-	test_routine("555555", 555555);
-	test_routine("1000000", 1000000);
-	test_routine("99999999", 99999999);
-	test_routine("111111111", 111111111);
-	test_routine("2000000000", 2000000000);
+	ft_itoa_test_routine("0", 0);
+	ft_itoa_test_routine("42", 42);
+	ft_itoa_test_routine("666", 666);
+	ft_itoa_test_routine("2112", 2112);
+	ft_itoa_test_routine("54321", 54321);
+	ft_itoa_test_routine("555555", 555555);
+	ft_itoa_test_routine("1000000", 1000000);
+	ft_itoa_test_routine("99999999", 99999999);
+	ft_itoa_test_routine("111111111", 111111111);
+	ft_itoa_test_routine("2000000000", 2000000000);
 }
 
 void	ft_itoa_negative_numbers_tests(void) {
-	test_routine("-1", -1);
-	test_routine("-42", -42);
-	test_routine("-666", -666);
-	test_routine("-2112", -2112);
-	test_routine("-54321", -54321);
-	test_routine("-555555", -555555);
-	test_routine("-1000000", -1000000);
-	test_routine("-99999999", -99999999);
-	test_routine("-111111111", -111111111);
-	test_routine("-2000000000", -2000000000);
+	ft_itoa_test_routine("-1", -1);
+	ft_itoa_test_routine("-42", -42);
+	ft_itoa_test_routine("-666", -666);
+	ft_itoa_test_routine("-2112", -2112);
+	ft_itoa_test_routine("-54321", -54321);
+	ft_itoa_test_routine("-555555", -555555);
+	ft_itoa_test_routine("-1000000", -1000000);
+	ft_itoa_test_routine("-99999999", -99999999);
+	ft_itoa_test_routine("-111111111", -111111111);
+	ft_itoa_test_routine("-2000000000", -2000000000);
 }
 
 void	ft_itoa_limits_numbers_tests(void) {
-	test_routine("2147483647", INT_MAX);
-	test_routine("-2147483648", INT_MIN);
+	ft_itoa_test_routine("2147483647", INT_MAX);
+	ft_itoa_test_routine("-2147483648", INT_MIN);
 }
 
 void	ft_itoa_tests(void) {
