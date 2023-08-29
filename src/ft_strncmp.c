5d2f7fbfcd7ed752d3aff98e45a991f1220742b7
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2023-08-29 20:29:01 -0300

    fix: null pointer crash fix

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 22:07:24 by guferrei          #+#    #+#             */
-/*   Updated: 2021/06/04 19:14:15 by guferrei         ###   ########.fr       */
+/*   Updated: 2023/08/29 20:25:13 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	c;
 
 	c = 1;
-	if (n == 0)
+	if (n == 0 || (!s1 || !s2))
 		return (0);
 	while (c < n && *s1 && *s2)
 	{
